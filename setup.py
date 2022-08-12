ab4821f71bce22d79185712799f863ad21c195b1
TheRealMizAL <50375436+TheRealMizAL@users.noreply.github.com>
2022-08-12 12:36:41 +0500

    pypi repo created

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='DaLaS',
+    version='0.1.0',
+    packages=['dalas'],
+    url='https://github.com/TheRealMizAL/DaLaS',
+    license='GNU GPL v3.0',
+    author='kov20',
+    author_email='amizurenko2002@gmail.com',
+    description='Fully customizable asynchronous Donation Alerts API',
+    install_requires=[
+        'pydantic>=1.9.1',
+        'loguru>=0.6.0',
+        'PyYAML>=6.0',
+        'websockets>=10.3',
+        'aiohttp>=3.8.1'
+    ]
+)
